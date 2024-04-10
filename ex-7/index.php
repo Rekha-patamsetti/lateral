fab156961293dde9baefdeae2fe091a9aa2c7b92
Rekha-patamsetti <yurekha7339@gamil.com>
2024-04-10 05:41:47 +0530

    firsthalf

---
+++ ex-7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Web Page</title>
+</head>
+<body>
+    <h1>This is a Heading</h1>
+    <p>
+        This is a sample paragraph of text. You can customize this content based on your needs.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+</body>
+</html>
